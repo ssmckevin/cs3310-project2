f77eb746e8ea235b140200b1f73acc0fc7f51ab0
ssmckevin <150875989+ssmckevin@users.noreply.github.com>
2025-12-07 01:15:45 -0800

    Update floyd

--- floyd_warshall.py
+++ floyd_warshall.py
@@ -1,6 +1,31 @@
 import networkx as nx
-import heapq
+from math import inf
 
 def apsp_length(G: nx.DiGraph):
+    nodes = list(G.nodes())
+    n = len(nodes)
+    idx = {nodes[i]: i for i in range(n)}
+
+    dist = [[inf]*n for _ in range(n)]
+
+    for i in range(n):
+        dist[i][i] = 0
+
+    for u, v, w in G.edges(data="weight"):
+        dist[idx[u]][idx[v]] = min(dist[idx[u]][idx[v]], w)
+
+    for k in range(n):
+        for i in range(n):
+            if dist[i][k] == inf:
+                continue
+            for j in range(n):
+                new_cost = dist[i][k] + dist[k][j]
+                if new_cost < dist[i][j]:
+                    dist[i][j] = new_cost
+
     paths = []
+    for i in range(n):
+        for j in range(n):
+            paths.append(dist[i][j])
+
     return paths
